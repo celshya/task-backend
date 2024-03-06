2dc6c82a15a5d2f980273fde9623cea6ac89b0da
Aman <aman.kumar@criodo.com>
2024-03-06 21:16:56 +0530

    Session 3 commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 //Connect to MongoDB
 mongoose
---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const taskController = require("../controllers/taskController");
+const authenticateToken = require("../middleware/authenticateToken");
+
+//Create a new task
+router.post("/", authenticateToken, taskController.createTask);
+
+// Get all tasks for the logged-in user
+router.get("/", authenticateToken, taskController.getAllTasks);
+
+//Get a specific task by ID
+router.get("/:id", authenticateToken, taskController.getTaskById);
+
+//Update a task by ID
+router.put("/:id", authenticateToken, taskController.updateTask);
+
+// Delete a task by ID
+router.delete("/:id", authenticateToken, taskController.deleteTask);
+
+module.exports = router;
